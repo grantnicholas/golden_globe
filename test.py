3784adecc965078d29ad441fb1d460e166156899
grant nicholas <grantnicholas2015@u.northwestern.edu>
2015-01-27 10:51:28 -0600

    end of class

--- test.py
+++ test.py
@@ -1,17 +1,46 @@
 from nltk.tokenize import word_tokenize
-import nltk
 import json
 
-nltk.download()
+IMPORTANT_WORDS = ['host', 'winner', 'presenter', 'nominees']
+def count_tokens(tokens):
+	token_dict = {}
+	for t in tokens:
+		if t in token_dict:
+			token_dict[t]+=1
+		else:
+			token_dict[t] = 1
 
-count = 0
-importantobjects = {}
-filename = './goldenglobes.json'
-with open(filename, 'r') as f:
-	for line in f:
-		if count>1000:
-			break;
-		text = json.loads(line)['text']
-		tokens = word_tokenize(text)
-		print tokens
-		count+=1
+	return token_dict
+
+def search_tokens(token_dict, vallist):
+
+def main():
+	count = 0
+	
+	filename = './goldenglobes.json'
+	with open(filename, 'r') as f:
+		for line in f:
+			if count>1000:
+				break;
+			text = json.loads(line)['text'].lower()
+			tokens = word_tokenize(text)
+			print count_tokens(tokens)
+
+
+
+
+
+
+
+
+
+
+
+
+			count+=1
+
+
+
+
+if __name__ == "__main__":
+	main()
